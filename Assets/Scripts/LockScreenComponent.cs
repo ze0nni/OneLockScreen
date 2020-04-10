df0a83f035ca264f194dd48505b1ebecda4cc235
e.blagodarev <ze0nni@gmail.com>
2020-04-10 16:17:00 +0300

    Add 'LockScreenComponent' - Placement dots on screen

---
+++ Assets/Scripts/LockScreenComponent.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Canvas))]
+public class LockScreenComponent : MonoBehaviour
+{
+    public Sprite CommonDot;
+    public Sprite ActiveDot;
+    public Sprite RejectedDot;
+
+    public float DotsDistance = 5f;
+
+    readonly private int size = 3;
+
+    void Start() {
+        var canvasSize = GetComponent<RectTransform>().rect.size;
+
+        var dotsOrigin = new Vector2(
+            (canvasSize.x - (size - 1) *DotsDistance) / 2f,
+            (canvasSize.y - (size - 1) * DotsDistance) / 2f
+        );
+
+        for (var x = 0; x < size; x ++) {
+            for (var y = 0; y < size; y++) {
+                //TODO: Prefab
+                var dotGo = new GameObject("Dot", typeof(Image), typeof(LocketScreenDotComponent));
+                dotGo.transform.SetParent(this.transform, false);
+
+                var dot = dotGo.GetComponent<LocketScreenDotComponent>();
+
+                var dotImage = dotGo.GetComponent<Image>();
+                dotImage.sprite = CommonDot;
+
+                var transform = dotGo.GetComponent<RectTransform>();
+                transform.position = new Vector2(x, y) * DotsDistance + dotsOrigin; ;
+            }
+        }
+    }
+}
