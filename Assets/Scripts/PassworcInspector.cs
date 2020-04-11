85b540a6353d4f446bec8dfc2cddb9c71271eea8
e.blagodarev <ze0nni@gmail.com>
2020-04-11 13:23:45 +0300

    Add 'PassworcInspector' for validate password

---
+++ Assets/Scripts/PassworcInspector.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+using MoonSharp.Interpreter;
+
+public class PassworcInspector : MonoBehaviour
+{
+    private static readonly string ResolverFuncBody = @"
+        return function(reject, resolve)
+            
+            local password = { 8, 5, 2, 1, 0, 3, 6, 7 }
+
+            return function(value)
+                if #value > #password then
+                    reject()
+                    return
+                end
+
+                for i=1,#value do
+                    if value[i] != password[i] then
+                        reject()
+                        return
+                    end
+                end
+
+                if #value == #password then
+                    resolve()
+                else 
+                    reject()
+                end
+            end
+        end
+    ";
+
+    public RopeComponent rope;
+
+    private Script script;
+    private DynValue ResolverFunc;
+
+    private IDisposable subscribe;
+
+    void OnEnable() {
+        this.script = new Script();
+        this.ResolverFunc = script.Call(script.DoString(ResolverFuncBody),
+            (Action)rope.Reject,
+            (Action)rope.Resolve
+        );
+
+
+        this.subscribe = rope.password.Subscribe(PasswordHandler);
+    }
+    
+    void OnDisable() {
+        subscribe.Dispose();
+    }
+
+    void PasswordHandler(int[] password) {
+        script.Call(ResolverFunc, password);
+    }
+}
